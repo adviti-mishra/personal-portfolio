e5f0a227bc487b3092847a65ffc1fe7372c15f0d
adviti <adviti@umich.edu>
2023-08-12 15:39:02 +0530

    Navbar closes on navigating to another page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
+  // Whether or not the Navbar is expanded
+  const [expandNavBar, setExpandNavbar] = useState(false);
+  // Information about which route you're in
+  const location = useLocation();
+  // whenever the location changes, set expandNavBar to false
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
   return (
-    <div className="navbar">
+    <div className="navbar" id={expandNavBar ? "open" : "close"}>
       <div className="toggleButton">
-        <button> </button>
+        <button onClick={() => setExpandNavbar(!expandNavBar)}>
+          <ReorderIcon />
+        </button>
       </div>
       <div className="links">
         <Link to={"/"}> Home </Link>
